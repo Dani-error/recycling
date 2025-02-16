8e294ca9cfa9e298e841c7c6e98dfcf606af36cf
Dani ツ <61289405+Dani-error@users.noreply.github.com>
2025-02-16 20:52:27 +0000

    feat: more and proper items

--- src/consts.ts
+++ src/consts.ts
@@ -1,15 +1,23 @@
 import { Bin, Difficulty, Item } from "./typings";
 
 export const items: Item[] = [
+    // Plastic Items
     { id: 1, type: "plastic", image: "./item/plastic-bottle.png" },
     { id: 2, type: "plastic", image: "/item/plastic-bag.png" },
     { id: 3, type: "plastic", image: "/item/plastic-cup.png" },
-    { id: 4, type: "glass", image: "/item/glass-bottle.png" },
-    { id: 5, type: "glass", image: "/item/glass-jar.png" },
-    { id: 6, type: "plastic", image: "/item/plastic-tube.png" },
-    { id: 7, type: "glass", image: "/item/glass-cup.png" },
-    { id: 8, type: "plastic", image: "/item/plastic-box.png" },
+    { id: 4, type: "plastic", image: "/item/metal-can.png" },
+
+    // Glass Items
+    { id: 5, type: "glass", image: "/item/glass-bottle.png" },
+    { id: 6, type: "glass", image: "/item/glass-jar.png" },
+    { id: 7, type: "glass", image: "/item/wine-bottle.png" },
+    { id: 8, type: "glass", image: "/item/jam-jar.png" },
+
+    // Paper Items
     { id: 9, type: "paper", image: "/item/paper-bag.png" },
+    { id: 10, type: "paper", image: "/item/newspaper.png" },
+    { id: 11, type: "paper", image: "/item/cardboard-box.png" },
+    { id: 12, type: "paper", image: "/item/paper-ball.png" },
 ];
 
 export const bins: Bin[] = [
@@ -19,7 +27,7 @@ export const bins: Bin[] = [
 ]
 
 export const difficulties: Difficulty[] = [
-    { name: "Easy", maxHealth: 3, maxTime: 30, maxItems: 9 },
-    { name: "Medium", maxHealth: 2, maxTime: 20, maxItems: 9 },
-    { name: "Hard", maxHealth: 1, maxTime: 15, maxItems: 9 }
+    { name: "Easy", maxHealth: 3, maxTime: 30, maxItems: 12 },
+    { name: "Medium", maxHealth: 2, maxTime: 20, maxItems: 12 },
+    { name: "Hard", maxHealth: 1, maxTime: 15, maxItems: 12 }
 ]
